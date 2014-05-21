e8e75dc4260e108d5709255879fa54947ccdd896
awaybrave <awaybrave@gmail.com>
2014-05-21 23:09:39 +0800

    deal with define

--- require.js
+++ require.js
@@ -6,12 +6,13 @@ var _moduleClass = function(){
 	dependedList = {}, // modules that are depended by others
 	dependList = {}, // modules that depend others
 	modules = {}, // save callbacks and require state.
-	last_require, last_define;
+	last_require, last_define, la;
 
 	this.setLastRequire = function(deps, callback){
 		last_require = {};
 		last_require['deps'] = deps;
 		last_require['callback'] = callback;
+		la = last_action['REQUIRE'];
 	};
 
 	this.setLastDefine = function(action){
@@ -19,42 +20,63 @@ var _moduleClass = function(){
 			last_define = action();
 		else
 			last_define = action;
-	};
+		la = last_action['DEFINE'];
+	}; 
 
-	this.messageParser = function(message){
+	this.setModuleLoaded = function(name){
+		if(la == last_action['DEFINE']){
+			modules[name] = last_define;
+			//release other modules.
+		}else{
 
-	};
-};
-
-var _moduleAction = function(){
-	this.require = function(){
-
-	};
-	this.define = function(){
-
+		}
 	};
 };
 
-var _scriptClass = function(){
-	var nameReg = /([^.]+.js^/;
+var _moduleAction = function(mm){
+	this.require = function(deps, callback){
+		mm.setLastRequire(deps, callback);
+	};
+	this.define = function(callback){
+		mm.setLastDefine(callback);
+	};
+};
+
+var _scriptClass = function(mm){
+	var nameReg = /([^\/.]+).js$/;
 	this.loadScript = function(path){
+		debugger;
 		var script_name = nameReg.exec(path)[1];
 		var script = document.createElement('script');
 		script.src = path;
 		script.name = script_name;
+		document.head.appendChild(script);
 		script.onload = function(){
 			//send message to modules manager with name.
+			mm.setModuleLoaded(this.name);
 		};
 	};
 };
 
-function(){
+(function(){
 	var _module_manager = new _moduleClass();
-	var _module_action = new _moduleAction();
-	var _scripts = new scriptClass();
+	var _module_action = new _moduleAction(_module_manager);
+	var _scripts = new _scriptClass(_module_manager);
 	if(!window['require'] && !window['define']){
-		window['require'] = _module_action.require();
-		window['define'] = _module_action.define();
+		window['require'] = _module_action.require;
+		window['define'] = _module_action.define;
 	}
+	else
+		return;
 	//load data-main
-}();
+	var scripts = document.getElementsByTagName("script");
+	var reg = /require.js$/;
+	for(var i = 0; i < scripts.length; i++){
+		if(reg.test(scripts[i].src)){
+			var mainScript = document.createElement("script");
+			var path = scripts[i].getAttribute("data-main");
+			_scripts.loadScript(path);
+			break;
+		}
+	}
+})();
